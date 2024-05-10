a07c9f85fa8755b545c7bdee13e92a0c48a69ed6
Suchit G <suchitg04@gmail.com>
2024-05-10 17:44:04 +0530

    Create find_user method in database.py to eliminated redundancy

--- app/database.py
+++ app/database.py
@@ -8,3 +8,6 @@ MONGODB_URI = os.getenv("MONGODB_URI")
 
 client = AsyncIOMotorClient(MONGODB_URI)
 db = client.nikata_test
+
+async def find_user(user_name: str):
+    return await db.users.find_one({"name": user_name})
