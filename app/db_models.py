734028223451ae15420ae39a9fff356769534a83
Suchit G <suchitg04@gmail.com>
2024-05-19 19:16:47 +0530

    Remove GeoJSONPoint model

--- app/db_models.py
+++ app/db_models.py
@@ -14,39 +14,33 @@ class UserModel(BaseModel):
     """
     Container for a single user record.
     """
-
     id: PyObjectId | None = Field(alias="_id", default=None)
     name: str
     email: EmailStr
     chat_ids: list[int]
     geofence_ids: list[PyObjectId]
 
-class GeoJSONPoint(BaseModel):
-    type: str = "Point"
-    coordinates: tuple[float, float]
-
-    @field_validator('coordinates')
-    def validate_coordinates(cls, v):
-        if not isinstance(v, tuple) or len(v) != 2:
-            raise ValueError("Coordinates must be a tuple with two float values")
-        longitude, latitude = v
-        if not (-180 <= longitude <= 180) or not (-90 <= latitude <= 90):
-            raise ValueError("Invalid coordinates: Longitude must be between -180 and 180, and latitude must be between -90 and 90 degrees")
-        return v
 
 class GeoFenceModel(BaseModel):
     """
     Container for a single geo fence record.
     """
-
     id: PyObjectId | None = Field(alias="_id", default=None)
     name: str
-    coordinates: GeoJSONPoint
+    # PyMongo retrieves the "Array" datatype from the DB as a list. So, we convert it into a tuple here.
+    coordinates: Annotated[tuple[float, float], BeforeValidator(tuple)]
     radius: float
+
+    @field_validator('coordinates')
+    def validate_coordinates(cls, v):
+        longitude, latitude = v
+        if not (-180 <= longitude <= 180) or not (-90 <= latitude <= 90):
+            raise ValueError("Invalid coordinates: Longitude must be between -180 and 180, and latitude must be between -90 and 90 degrees")
+        return v
+
 
 class GeoFenceCollection(BaseModel):
     """
     Container for a list of 'GeoFenceModel' instances.
     """
-
     geofences: list[GeoFenceModel]
